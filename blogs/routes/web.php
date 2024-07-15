cab7c203186eb8863ff0a47e3ed501a38e231135
mohamedaymn <mohamedaymn218@gmail.com>
2024-07-16 00:47:04 +0300

    chore: profile page components

--- blogs/routes/web.php
+++ blogs/routes/web.php
@@ -5,9 +5,11 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SignUpController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/blog/{id}', [BlogController::class, 'index']);
 Route::get('/login', [LoginController::class, 'index']);
 Route::get('/signup', [SignUpController::class, 'index']);
+Route::get('/profile/{id}', [ProfileController::class, 'index']);
 
