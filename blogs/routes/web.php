42ce3fe669aea9896f285e3fcd5db2372bb8500c
mohamedaymn <mohamedaymn218@gmail.com>
2024-07-16 00:42:30 +0300

    chore: sign up page

--- blogs/routes/web.php
+++ blogs/routes/web.php
@@ -4,8 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignUpController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/blog/{id}', [BlogController::class, 'index']);
 Route::get('/login', [LoginController::class, 'index']);
+Route::get('/signup', [SignUpController::class, 'index']);
 
