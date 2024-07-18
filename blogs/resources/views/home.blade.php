a3e3b94638ff9672dca76e920fb91cb865bfd481
mohamedaymn <mohamedaymn218@gmail.com>
2024-07-18 16:35:34 +0300

    chore: updated footer content

--- blogs/resources/views/home.blade.php
+++ blogs/resources/views/home.blade.php
@@ -12,7 +12,7 @@
                             <path d="m21 21-4.3-4.3"></path>
                         </svg>
                     </div>
-                    <input class="py-3 ps-10 pe-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none " type="text" placeholder="Type a name" value="" data-hs-combo-box-input="">
+                    <input class="py-3 ps-10 pe-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none " type="text" placeholder="Search for a blog" value="" data-hs-combo-box-input="">
                 </div>
             
             </div>
--- blogs/resources/views/partials/header.blade.php
+++ blogs/resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4">
     <nav class="max-w-[85rem] w-full mx-auto px-6 sm:flex sm:items-center sm:justify-between" aria-label="Global">
         <div class="flex items-center justify-between">
-        <a class="flex-none text-xl font-semibold " href="#">Brand</a>
+        <a class="flex-none text-xl font-semibold " href="/">Blogs</a>
         <div class="sm:hidden">
             <button type="button" class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" data-hs-collapse="#navbar-with-collapse" aria-controls="navbar-with-collapse" aria-label="Toggle navigation">
             <svg class="hs-collapse-open:hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
