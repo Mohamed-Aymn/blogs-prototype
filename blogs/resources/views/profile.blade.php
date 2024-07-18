b9d43edb3f8808d3119077192a1f9415e7853041
mohamedaymn <mohamedaymn218@gmail.com>
2024-07-18 21:44:07 +0300

    chore: added button for node connection

--- blogs/resources/views/profile.blade.php
+++ blogs/resources/views/profile.blade.php
@@ -7,10 +7,14 @@
         <img class="w-32 h-32 rounded-full mx-auto" src="https://picsum.photos/200" alt="Profile picture">
         <h2 class="text-center text-2xl font-semibold mt-3">{{$user->name}}</h2>
         <p class="text-center text-gray-600 mt-1">{{$user->email}}</p>
-        <form action="{{ route('logout') }}" method="POST" class="w-full block text-right">
-            @csrf
-            <button type="submit" class="text-gray-500 text-right">Logout</button>
-        </form>
+
+        <div class="flex items-center w-full">
+            <a href="http://localhost:3000" class="text-gray-500 flex-grow">Create Post</a>
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="text-gray-500 text-right">Logout</button>
+            </form>
+        </div>
     </div>
 
 @endsection
