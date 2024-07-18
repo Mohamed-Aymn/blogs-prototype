5a2f730efc8c3f24574ee297a427ff25f031d9ee
mohamedaymn <mohamedaymn218@gmail.com>
2024-07-18 16:28:48 +0300

    fix: default layout

--- blogs/resources/views/partials/header.blade.php
+++ blogs/resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header class="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4">
-    <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+    <nav class="max-w-[85rem] w-full mx-auto px-6 sm:flex sm:items-center sm:justify-between" aria-label="Global">
         <div class="flex items-center justify-between">
         <a class="flex-none text-xl font-semibold " href="#">Brand</a>
         <div class="sm:hidden">
@@ -11,13 +11,10 @@
         </div>
         <div id="navbar-with-collapse" class="hidden transition-all duration-[0.1ms] overflow-hidden basis-full grow sm:block">
             <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-                <a class="font-medium text-blue-500" href="#" aria-current="page">Landing</a>
-                <a class="font-medium text-gray-600 hover:text-gray-400 " href="#">Account</a>
-                <a class="font-medium text-gray-600 hover:text-gray-400 " href="#">Work</a>
-                <a class="font-medium text-gray-600 hover:text-gray-400 " href="#">Blog</a>
                 <?php if (isAuthenticated()): ?>
+                    <a class="font-medium text-gray-600 hover:text-gray-400 " href="#">Profile</a>
                     <form action="<?php echo route('logout'); ?>" method="POST" class="inline">
-                        <?php echo csrf_field(); ?>
+                        @csrf
                         <button type="submit" class="text-blue-500">Logout</button>
                     </form>
                 <?php else: ?>
