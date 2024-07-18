b98bad93e7878d2d7d4bf895727748d4b5a11af8
mohamedaymn <mohamedaymn218@gmail.com>
2024-07-19 01:09:26 +0300

    feat: editor input items generation

--- editor/ui/src/Routes/Editor.tsx
+++ editor/ui/src/Routes/Editor.tsx
@@ -1,16 +1,57 @@
-import Footer from "../components/Footer"
-import Header from "../components/Header"
+import { useState, useRef, useEffect, KeyboardEvent } from 'react';
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import InputItem, { InputItemRef } from "../components/InputItem";
 
-function Editor() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <div className="flex-grow">
-        Editor
-      </div>
-      <Footer />
-    </div>
-  )
+interface InputItemType {
+	id: number;
 }
 
-export default Editor
+function Editor() {
+	const [inputItems, setInputItems] = useState<InputItemType[]>([{ id: 1 }]);
+	const inputRefs = useRef<(InputItemRef | null)[]>([]);
+
+	useEffect(() => {
+		if (inputRefs.current.length > 0) {
+			inputRefs.current[inputRefs.current.length - 1]?.focus();
+		}
+	}, [inputItems]);
+
+	const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>, index: number) => {
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			setInputItems(prev => [...prev, { id: prev.length + 1 }]);
+		} else if (e.key === 'Backspace' && e.currentTarget.value === '' && inputItems.length > 1) {
+			e.preventDefault();
+			const newInputItems = inputItems.filter((_, i) => i !== index);
+			setInputItems(newInputItems);
+			inputRefs.current = inputRefs.current.filter((_, i) => i !== index);
+			// Focus the previous input
+			const previousIndex = index > 0 ? index - 1 : 0;
+			inputRefs.current[previousIndex]?.focus();
+		}
+	};
+
+	return (
+		<div className="flex flex-col min-h-screen">
+			<Header />
+			<div className="flex-grow w-full">
+				<div className="mx-auto px-6 max-w-[85rem]">
+					<input className="text-4xl border-0 outline-none py-1 w-full" placeholder="Blog Title" />
+					<div id="content" className="mt-10">
+						{inputItems.map((item, index) => (
+							<InputItem
+								key={item.id}
+								onKeyDown={(e) => handleKeyDown(e, index)}
+								ref={el => inputRefs.current[index] = el}
+							/>
+						))}
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
+}
+
+export default Editor;
---
+++ editor/ui/src/components/InputItem.tsx
@@ -0,0 +1,29 @@
+import { forwardRef, KeyboardEvent } from 'react';
+
+interface InputItemProps {
+    onKeyDown: (e: KeyboardEvent<HTMLInputElement>) => void;
+}
+
+export type InputItemRef = HTMLInputElement;
+
+const InputItem = forwardRef<InputItemRef, InputItemProps>(({ onKeyDown }, ref) => {
+    return (
+        <div className="relative group">
+            <svg
+                className="absolute -left-5 top-[0.1em] size-5 opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none" viewBox="0 0 24 24" stroke="currentColor"
+            >
+                <path d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+            </svg>
+            <input
+                className="item w-full border-0 outline-none"
+                placeholder="Type here!"
+                onKeyDown={onKeyDown}
+                ref={ref}
+            />
+        </div>
+    );
+});
+
+export default InputItem;
