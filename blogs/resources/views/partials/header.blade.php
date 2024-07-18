0d10485e63040b978ab86b9b6d288b63457973e8
mohamedaymn <mohamedaymn218@gmail.com>
2024-07-18 17:54:44 +0300

    chore: remove unused mobile navbar

--- blogs/resources/views/partials/header.blade.php
+++ blogs/resources/views/partials/header.blade.php
@@ -3,9 +3,32 @@
     $profileRoutePattern = '/^\/profile\/\d+$/';
 ?>
 
-<header class="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4">
-    <nav class="max-w-[85rem] w-full mx-auto px-6 sm:flex sm:items-center sm:justify-between" aria-label="Global">
-        <div class="flex items-center justify-between">
+<header class="flex flex-wrap w-full bg-white text-sm py-4">
+    <nav class="max-w-[85rem] w-full mx-auto px-6 flex items-center justify-between" aria-label="Global">
+        <a class="flex-none text-xl font-semibold " href="/">Blogs</a>
+
+        <div id="navbar-with-collapse" class="transition-all">
+            @if (isAuthenticated())
+                @if (preg_match($profileRoutePattern, $currentRoute))
+                    <a class="font-medium text-gray-600 hover:text-gray-400" href="/">Home</a>
+                @else
+                    @php
+                        $user = getUserDataFromToken();
+                    @endphp
+                    <a class="font-medium text-gray-600 hover:text-gray-400" href="{{ $user->id ? '/profile/' . $user->id : '#' }}">Profile</a>
+                @endif
+            @else
+                <a class="font-medium text-gray-600 hover:text-gray-400" href="/login">Login</a>
+            @endif
+        </div>
+
+        
+
+
+        {{-- responsive navbar --}}
+
+        {{-- mobile icon --}}
+        {{-- <div class="flex items-center justify-between">
             <a class="flex-none text-xl font-semibold " href="/">Blogs</a>
             <div class="sm:hidden">
                 <button type="button" class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" data-hs-collapse="#navbar-with-collapse" aria-controls="navbar-with-collapse" aria-label="Toggle navigation">
@@ -13,8 +36,10 @@
                     <svg class="hs-collapse-open:block hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
                 </button>
             </div>
-        </div>
-        <div id="navbar-with-collapse" class="hidden transition-all duration-[0.1ms] overflow-hidden basis-full grow sm:block">
+        </div> --}}
+
+        {{-- big screen navbar --}}
+        {{-- <div id="navbar-with-collapse" class="hidden transition-all duration-[0.1ms] overflow-hidden basis-full grow sm:block">
             <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
                 @if (isAuthenticated())
                     @if (preg_match($profileRoutePattern, $currentRoute))
@@ -29,6 +54,6 @@
                     <a class="font-medium text-gray-600 hover:text-gray-400" href="/login">Login</a>
                 @endif
             </div>
-        </div>
+        </div> --}}
     </nav>
 </header>
