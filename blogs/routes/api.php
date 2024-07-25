a3ecf98fbe14a6a9462bff96af1eec9eb6b619d4
mohamedaymn <mohamedaymn218@gmail.com>
2024-07-26 01:53:31 +0300

    feat: fetch posts api

--- blogs/routes/api.php
+++ blogs/routes/api.php
@@ -4,13 +4,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 
-Route::get('/posts', function (Request $request) {
-    $url = env('NODE_URL') . '/editor/api/posts';
+Route::prefix('posts')->group(function () {
+    Route::get('/', function (Request $request) {
+        $url = env('NODE_URL') . '/editor/api/posts';
+        try {
+            $response = Http::get($url);
+            return response()->json($response->json());
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Unable to fetch data from external service'], 500);
+        }
+    });
 
-    try {
-        $response = Http::get($url);
-        return response()->json($response->json());
-    } catch (\Exception $e) {
-        return response()->json(['error' => 'Unable to fetch data from external service'], 500);
-    }
+    Route::get('/{id}', function ($id) {
+        $url = env('NODE_URL') . "/editor/api/posts/{$id}";
+        try {
+            $response = Http::get($url);
+            return response()->json($response->json());
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'unable to fetch post'], 500);
+        }
+    });
 });
