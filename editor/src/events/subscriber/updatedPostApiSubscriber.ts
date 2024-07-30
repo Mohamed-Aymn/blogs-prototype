02c9bbc3b668ee07aba8a5a37cbb0ef3e1b2b2b4
mohamedaymn <mohamedaymn218@gmail.com>
2024-07-30 15:59:48 +0300

    fix: post updated event

--- editor/src/events/subscriber/updatedPostApiSubscriber.ts
+++ editor/src/events/subscriber/updatedPostApiSubscriber.ts
@@ -1,6 +1,6 @@
-import { createPost } from '../../persistence/repositories/postRepository';
+import { updatePost } from '../../persistence/repositories/postRepository';
 import { IPost } from '../../persistence/types/post';
-import { createdPostPublisher } from '../publisher/createdPostPublisher';
+import { updatedPostPublisher } from '../publisher/updatedPostPublisher';
 import redisClient from '../redisSubClient';
 import { IPostUpdatedEvent } from '../types/postUpdatedEvent';
 
@@ -8,8 +8,8 @@ export const updatedPostApiSubscriber = () => {
     redisClient.subscribe('updated-post-api', async (message) => {
         try {
             const newPost = JSON.parse(message) as IPost;
-            await createPost(newPost); 
-            await createdPostPublisher(newPost as IPostUpdatedEvent);
+            await updatePost(String(newPost._id), newPost); 
+            await updatedPostPublisher(newPost as IPostUpdatedEvent);
         } catch (error) {
             console.error('Failed to update post from message:', message, error);
         }
