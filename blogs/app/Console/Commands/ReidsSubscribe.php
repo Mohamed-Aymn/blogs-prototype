be53d627f7e970dd5b17bbf684f933f373e14fbb
mohamedaymn <mohamedaymn218@gmail.com>
2024-07-30 16:09:46 +0300

    refactor: laravel subscriber

--- blogs/app/Console/Commands/ReidsSubscribe.php
+++ blogs/app/Console/Commands/ReidsSubscribe.php
@@ -7,8 +7,7 @@ use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Log;
 use App\Models\Post;
 
-
-class ReidsSubscribe extends Command
+class RedisSubscribe extends Command
 {
     /**
      * The name and signature of the console command.
@@ -22,7 +21,7 @@ class ReidsSubscribe extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Subscribe to Redis channels and handle messages.';
 
     public function __construct()
     {
@@ -34,22 +33,94 @@ class ReidsSubscribe extends Command
      */
     public function handle()
     {
-        $this->info('start handling');
-        Redis::subscribe(['post-created'], function ($message) {
+        $this->info('Starting Redis subscription...');
+        Redis::subscribe(['post-created', 'post-updated', 'post-deleted'], function (string $message, string $channel) {
             $data = json_decode($message, true);
-            $this->info('Received message: ' . print_r($data, true));
+            $this->info('Received message on ' . $channel . ': ' . print_r($data, true));
 
             try {
-                $post = Post::create([
-                    'user_id' => $data['userId'], 
-                    'id' => $data['_id'], 
-                    'avg_read_time' => 3,
-                    'title' => $data['title'],
-                    'body' => $data['data'][0]['data'],
-                ]);
+                $this->handleChannelMessage($channel, $data);
             } catch (\Exception $e) {
-                $this->info('error: ' . print_r($e, true));
+                $this->error('Error: ' . $e->getMessage());
             }
         });
+    }
+
+    /**
+     * Handle the message according to the channel.
+     *
+     * @param string $channel
+     * @param array $data
+     */
+    protected function handleChannelMessage(string $channel, array $data)
+    {
+        $channelActions = $this->getChannelActions();
+
+        if (array_key_exists($channel, $channelActions)) {
+            $channelActions[$channel]($data);
+        } else {
+            $this->info('No handler defined for channel: ' . $channel);
+        }
+    }
+
+    /**
+     * Get the mapping of channels to actions.
+     *
+     * @return array
+     */
+    protected function getChannelActions(): array
+    {
+        return [
+            'post-created' => function ($data) {
+                $this->createPost($data);
+            },
+            'post-updated' => function ($data) {
+                $this->updatePost($data);
+            },
+            'post-deleted' => function ($data) {
+                $this->deletePost($data);
+            },
+        ];
+    }
+
+    /**
+     * Create a new post from the received data.
+     *
+     * @param array $data
+     */
+    protected function createPost(array $data)
+    {
+        Post::create([
+            'user_id' => $data['userId'],
+            'id' => $data['_id'],
+            'avg_read_time' => 3,
+            'title' => $data['title'],
+            'body' => $data['data'][0]['data'],
+        ]);
+    }
+
+    /**
+     * Update an existing post with the received data.
+     *
+     * @param array $data
+     */
+    protected function updatePost(array $data)
+    {
+        Post::where('id', $data['_id'])->update([
+            'user_id' => $data['userId'],
+            'avg_read_time' => 3,
+            'title' => $data['title'],
+            'body' => $data['data'][0]['data'],
+        ]);
+    }
+
+    /**
+     * Delete a post with the received data.
+     *
+     * @param array $data
+     */
+    protected function deletePost(array $data)
+    {
+        Post::where('id', $data['_id'])->delete();
     }
 }
