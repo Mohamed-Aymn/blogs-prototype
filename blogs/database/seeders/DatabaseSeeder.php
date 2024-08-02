6544003c74ef15c6b915bcb1d88a7b40e0c2dc75
mohamedaymn <mohamedaymn218@gmail.com>
2024-08-02 21:07:22 +0300

    chore: mysql seed data

--- blogs/database/seeders/DatabaseSeeder.php
+++ blogs/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create([
+            'name' => 'Mohamed Aymn Khanfour',
+            'email' => 'mohamedaymn218@gmail.com',
+            'password' => Hash::make("12345678"),
         ]);
+
+        DB::transaction(function () use ($user) {
+            Post::create([
+                'id' => "64c5b2fbcf85e6c0c9fe5a1a",
+                'title' => "Test Post",
+                'user_id' => $user->id,
+                'avg_read_time' => 3,
+                'body' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the", 
+            ]);
+        
+            Post::create([
+                'id' => "64c5b2fbcf85e6c0c9fe5a1b",
+                'title' => "Second Test Post",
+                'user_id' => $user->id,
+                'avg_read_time' => 5,
+                'body' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the", 
+            ]);
+        });
     }
 }
